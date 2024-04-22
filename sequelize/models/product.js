23a2c7924e888845f797a4f3144cfb0a7ed95f01
bhargavatish <139468145+bhargavatish@users.noreply.github.com>
2024-04-22 23:23:51 +0530

    using SEQUELIZE first time

---
+++ sequelize/models/product.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize');
+
+const sequelize = require('../util/database');// to create the connection pool
+// Now defining model that could be managed by sequelize
+const Product = sequelize.define('products', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+module.exports = Product;
