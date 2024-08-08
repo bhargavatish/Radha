4fe24edde727d349b93388f7fdee39d9c69474fb
bhargavatish <139468145+bhargavatish@users.noreply.github.com>
2024-08-08 18:01:42 +0530

    It's time to make money

---
+++ Integrating Razorpay/Backend/Model/User.js
@@ -0,0 +1,31 @@
+const Sequelize = require('sequelize')
+
+const sequelize = require('../Util/Database')
+
+const User = sequelize.define('user',{
+    id : {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey:true
+    },
+    names : {
+        type: Sequelize.STRING,
+        allowNull : false
+    },
+    email: {
+        type : Sequelize.STRING,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    isPremiumUser:{
+        type:Sequelize.BOOLEAN
+        
+    }
+    
+})
+
+module.exports = User ;
